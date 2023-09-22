8a92ef428f3f8c083aba43a238a20fba1cdee3fa
Dennis M Fashimpaur <Fashimpaur@users.noreply.github.com>
2023-09-22 13:22:20 -0500

    Saving files before refreshing line endings

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM tiangolo/uwsgi-nginx:python3.11
+FROM python:3.11-alpine
 WORKDIR /code
 ENV STATIC_URL /static
 ENV STATIC_PATH /code/static
-COPY requirements.txt /code/requirements.txt
+COPY requirements.txt /code
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt --no-cache-dir
-COPY * /code
-CMD python ./flask_server/app.py
+COPY ./flask_server /code
+CMD python app.py
--- flask_server/app.py
+++ flask_server/app.py
@@ -1,10 +1,14 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route('/make_pdf/', methods=['GET'])
-# def create_pdf_document():
-#     return 'Success'
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
+import os
+
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+
+
+@app.route('/make_pdf/', methods=['GET'])
+def create_pdf_document():
+    return 'Success'
+
+
+if __name__ == '__main__':
+    app.run()
